ba471304e5aa86b9f6efe5285d2e0af415a31dea
Luca Cavallin <me@lucavall.in>
2018-07-22 13:04:49 +0200

    json,. mqtt

--- yun.ino
+++ yun.ino
@@ -1,69 +1,109 @@
-#include <DHT.h>
-#include <Ultrasonic.h>
-#include <Bridge.h>
-#include <BridgeServer.h>
-#include <BridgeClient.h>
+#include <ArduinoJson.h>
 
-#define DHT_PIN 4
-#define DHT_TYPE DHT11
-#define BUZZER_PIN 8
-#define LED_BLUE_PIN 3
-#define ULTRASONIC_RANGER_PIN 2
+#include <MQTTClient.h>
+#include <MQTT.h>
+#include <system.h>
 
-DHT dht(DHT_PIN, DHT_TYPE);
-Ultrasonic ultrasonic(ULTRASONIC_RANGER_PIN);
-BridgeServer server;
+#include "Ultrasonic.h"
+#include "DHT.h"
+
+#define DHTTYPE DHT11
+
+MQTTClient client;
+int ultrasonicPin = 2;
+int DHTPin = 4;
+int buzzerPin = 8;
+int ledPin = 3;
+
+DHT dht(DHTPin, DHTTYPE);
+Ultrasonic ultrasonic(ultrasonicPin);
+
+void connect()
+{
+  Serial.print("\nConnecting to MQTT...");
+  while (!client.connect("arduino", "try", "try"))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload)
+{
+  Serial.println("incoming: " + topic + " - " + payload);
+}
 
 void setup()
 {
-  // Sensors setup
-  dht.begin();
-  pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(LED_BLUE_PIN, OUTPUT);
+  // MQTT
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(messageReceived);
+  connect();
 
-  // External and openwrt communications setup
-  Bridge.begin();
-  server.begin();
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
-  BridgeClient client = server.accept();
-
-  if (client) {
-    process(client);
-    client.stop();
+  // MQTT
+  client.loop();
+  if (!client.connected())
+  {
+    connect();
   }
 
+  long ultrasonicCentimeters;
+
+  ultrasonicCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+  Serial.println(ultrasonicCentimeters);
+  if (ultrasonicCentimeters > 50)
+  {
+    toggleDigital(buzzerPin, 250);
+  }
+  else
+  {
+    toggleDigital(ledPin, 1000);
+  }
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h))
+  {
+    Serial.println("Failed to read from DHT");
+  }
+  else
+  {
+    printDHT(h, t);
+  }
+
+  Serial.println("\n");
   delay(1000);
 }
 
-void process(BridgeClient client) {
-  String command = client.readStringUntil('/');
-
-  if(command == "digital") {
-    toggleDigital(13, 1000);
-  }
-  if(command == "buzzer") {
-    toggleDigital(BUZZER_PIN, 1000);
-  }
-  if(command == "ultrasonic_ranger") {
-    long ultrasonicCentimeters = ultrasonic.MeasureInCentimeters();
-    client.write(ultrasonicCentimeters);
-  }
-  if(command == "humidity") {
-    float h = dht.readHumidity();
-    client.write(h);
-  }
-  if(command == "temperature") {
-    float t = dht.readTemperature();
-    client.write(t);
-  }
-}
-
-void toggleDigital(int pin, int delayTime) {
+void toggleDigital(int pin, int delayTime)
+{
   digitalWrite(pin, HIGH);
   delay(delayTime);
   digitalWrite(pin, LOW);
 }
 
+void printDHT(float h, int t)
+{
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print("\t Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C");
+}
