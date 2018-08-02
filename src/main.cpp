2f2f077e42558b84509c5d92878f19d2a120d6b4
Luca Cavallin <me@lucavall.in>
2018-08-02 11:49:50 +0200

    starting to parse json

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <BridgeClient.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <Time.h>
 
 // Networking
 BridgeClient connection;
@@ -21,17 +22,61 @@ DHT dht(DHTPin, DHTTYPE);
 Ultrasonic ultrasonic(ultrasonicPin);
 
 // Setup MQTT
+#define INCOMING_TOPIC "to-arduino"
+#define OUTGOING_TOPIC "from-arduino"
+
+void onMessageReceived(char *topic, byte *payload, unsigned int length);
+PubSubClient client(MQTT_HOSTNAME, MQTT_PORT, onMessageReceived, connection);
+
 void onMessageReceived(char *topic, byte *payload, unsigned int length)
 {
-    Serial.println("received");
-    Serial.println(topic);
-}
+    Serial.print("Message received from topic: ");
+    Serial.print(topic);
+    Serial.println();
 
-PubSubClient client(MQTT_HOSTNAME, MQTT_PORT, onMessageReceived, connection);
+    // Get and parse request
+    String json = (char *)payload;
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject &incomingRoot = jsonBuffer.parseObject(json);
+
+    // Test if parsing succeeds.
+    if (!incomingRoot.success())
+    {
+        Serial.println("Failed to parse request.");
+        return;
+    }
+
+    // Prepare response
+    const char *sensor = incomingRoot["sensor"];
+
+    JsonObject &outgoingRoot = jsonBuffer.createObject();
+    outgoingRoot["id"] = "1394y71289312";
+    outgoingRoot["timestamp"] = now();
+    outgoingRoot["sensor"] = sensor;
+    JsonArray &data = outgoingRoot.createNestedArray("data");
+
+    // TODO
+    // data.add(48.756080);
+    // data.add(2.302038);
+
+    // char *response;
+    // outgoingRoot.printTo(response, 128);
+
+    // Send response
+    Serial.print("Publishing message: ");
+    Serial.print(sensor);
+    Serial.print(" to topic: ");
+    Serial.print(OUTGOING_TOPIC);
+    Serial.print(".");
+    Serial.println();
+
+    client.publish("from-arduino", response);
+}
 
 // Program
 void setup()
 {
+    Serial.println("Setting up Bridge and Serial...");
     Bridge.begin();
     Serial.begin(115200);
 }
@@ -45,6 +90,7 @@ void loop()
         if (client.connect("hytta", MQTT_USERNAME, MQTT_PASSWORD))
         {
             Serial.println("Connected!");
+            client.subscribe(INCOMING_TOPIC);
         }
     }
 
