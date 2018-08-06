7065def3ddde9c8fa27577161e12b39431b0ed40
Luca Cavallin <me@lucavall.in>
2018-08-06 11:24:05 +0200

    cleaning up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,40 @@
 #include <Bridge.h>
 #include <BridgeClient.h>
 #include <ArduinoJson.h>
-#include <PubSubClient.h>
+#include <MQTT.h>
 #include <Time.h>
 
 // Networking
 BridgeClient connection;
+MQTTClient client;
 
 // Sensors and actuators libraries
 #include "Ultrasonic.h"
 #include "DHT.h"
-
-#define SENSOR_READ 0
-#define SENSOR_WRITE 1
-#define DHTTYPE DHT11
-
-#define PIN_ULTRASONIC 2
-#define PIN_DHT 4
-#define PIN_BUZZER 8
-#define PIN_LED 3
-#define PIN_DHT 4
-
-DHT dht(PIN_DHT, DHTTYPE);
+const byte SENSOR_READ = 0;
+const byte SENSOR_WRITE = 1;
+const byte PIN_ULTRASONIC = 2;
+const byte PIN_LED = 3;
+const byte PIN_DHT = 4;
+const byte PIN_BUZZER = 8;
+DHT dht(PIN_DHT, DHT11);
 Ultrasonic ultrasonic(PIN_ULTRASONIC);
 
 // Setup MQTT
-#define INCOMING_TOPIC "to-arduino"
-#define OUTGOING_TOPIC "from-arduino"
+const String INCOMING_TOPIC = "to-arduino";
+const String OUTGOING_TOPIC = "from-arduino";
 
-void onMessageReceived(char *topic, byte *payload, unsigned int length);
-String handleRequest(const char *sensor, char *params);
+String handleRequest(String sensor, char *params);
 void toggleDigital(int pin, int delayTime);
-PubSubClient client(MQTT_HOSTNAME, MQTT_PORT, onMessageReceived, connection);
 
-void onMessageReceived(char *topic, byte *payload, unsigned int length)
+void onMessageReceived(String &topic, String &payload)
 {
     // Debug message
-    Serial.print("Message received from topic: ");
-    Serial.print(topic);
-    Serial.println();
+    Serial.println(sprintf("Message received from topic: %s", topic.c_str()));
 
     // Get and parse request
-    String json = (char *)payload;
-    StaticJsonBuffer<MQTT_MAX_PACKET_SIZE> jsonBuffer;
+    String json = payload;
+    StaticJsonBuffer<sizeof(payload)> jsonBuffer;
     JsonObject &incomingRoot = jsonBuffer.parseObject(json);
 
     // Test if parsing succeeds.
@@ -53,7 +45,7 @@ void onMessageReceived(char *topic, byte *payload, unsigned int length)
     }
 
     // Prepare response and add metadata
-    const char *sensor = incomingRoot["sensor"];
+    String sensor = incomingRoot["sensor"];
     JsonObject &outgoingRoot = jsonBuffer.createObject();
     // @TODO find way to have a unique ID for the device
     outgoingRoot["id"] = "1394y71289312";
@@ -62,26 +54,36 @@ void onMessageReceived(char *topic, byte *payload, unsigned int length)
     outgoingRoot["data"] = handleRequest(sensor, incomingRoot["params"]);
 
     // Converting response to needed type
-    char response[MQTT_MAX_PACKET_SIZE];
-    outgoingRoot.printTo(response, sizeof(response));
+    String response;
+    outgoingRoot.printTo(response);
 
     // Send response, debug message
-    Serial.print("Publishing message: ");
-    Serial.print(sensor);
-    Serial.print(" to topic: ");
-    Serial.print(OUTGOING_TOPIC);
-    Serial.print(".");
-    Serial.println();
-
-    client.publish("from-arduino", response);
+    Serial.println(sprintf("Publishing message: %s to topic: %s.", sensor.c_str(), OUTGOING_TOPIC.c_str()));
+    client.publish(OUTGOING_TOPIC, response);
 }
 
 // Program
+void connect()
+{
+    Serial.println("Connecting to MQTT...");
+    while (!client.connect("hytta", MQTT_USERNAME, MQTT_PASSWORD))
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println("Connected!");
+
+    client.subscribe(INCOMING_TOPIC);
+}
+
 void setup()
 {
+    // Setup coms
     Serial.println("Setting up Bridge and Serial...");
     Bridge.begin();
     Serial.begin(115200);
+    client.begin(MQTT_HOSTNAME, MQTT_PORT, connection);
+    client.onMessage(onMessageReceived);
 
     // Setup Sensors
     dht.begin();
@@ -91,46 +93,39 @@ void setup()
 
 void loop()
 {
+    client.loop();
     // MQTT
     if (!client.connected())
     {
-        Serial.println("Connecting to MQTT...");
-        if (client.connect("hytta", MQTT_USERNAME, MQTT_PASSWORD))
-        {
-            Serial.println("Connected!");
-            client.subscribe(INCOMING_TOPIC);
-        }
+        connect();
     }
 
-    if (client.connected())
-    {
-        client.loop();
-    }
+    client.subscribe(INCOMING_TOPIC);
 }
 
 // This needs serious re-thought
-String handleRequest(const char *sensor, char *params)
+String handleRequest(String sensor, char *params)
 {
-    if (strcmp(sensor, "temperature") == 0)
+    if (sensor == "temperature")
     {
         Serial.println(dht.readTemperature());
         return (String)dht.readTemperature();
     }
-    else if (strcmp(sensor, "humidity") == 0)
+    else if (sensor == "humidity")
     {
         return (String)dht.readHumidity();
     }
-    else if (strcmp(sensor, "digital") == 0)
+    else if (sensor == "digital")
     {
         toggleDigital(3, 1000);
         return "success";
     }
-    else if (strcmp(sensor, "buzzer") == 0)
+    else if (sensor == "buzzer")
     {
         toggleDigital(PIN_BUZZER, 1000);
         return "success";
     }
-    else if (strcmp(sensor, "ultrasonic_ranger") == 0)
+    else if (sensor == "ultrasonic_ranger")
     {
         return (String)ultrasonic.MeasureInCentimeters();
     }
